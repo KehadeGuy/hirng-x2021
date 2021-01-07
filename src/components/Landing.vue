725c98a5c529c9786fad2deb86a13e6789f9ab86
Kehinde Ademola <kehade91@yahoo.com>
2021-01-07 22:43:09 +0100

    Initial commit

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,68 +1,73 @@
 <template>
-    <div v-if="userData">
-        <p class="heading__1">{{userData.name}}</p>
-        <div>
-            <img src="../assets/imagery.jpg" alt="img" class="imagery">
+    <div>
+        <div v-if="loading" class="loading">
+            <h1>Loading...</h1>
         </div>
-        <div class="footer_menu">
-            <div class="logo_area">
-                <a href="">
-                    <img src="../assets/face.jpg" alt="">
-                </a>
+        <div v-else>
+            <p class="heading__1">{{userData.name}}</p>
+            <div>
+                <img src="../assets/imagery.jpg" alt="img" class="imagery">
             </div>
-            <div class="footer_inner_main_menu">
-                <ul>
+            <div class="footer_menu">
+                <div class="logo_area">
+                    <a href="">
+                        <img src="../assets/face.jpg" alt="">
+                    </a>
+                </div>
+                <div class="footer_inner_main_menu">
+                    <ul>
+                        <li>
+                            <router-link to="/">
+                                Home
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/">
+                                About
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/">
+                                Gallery
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link to="/">
+                                Contact
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="sticky-container">
+                <ul class="sticky">
                     <li>
-                        <router-link to="/">
-                            Home
-                        </router-link>
+                        <a :href="`https://www.instagram.com/${userData.social_media.instagram}`" :title="`${userData.social_media.instagram}`" target="_blank" rel="noreferrer noopener">
+                            <img src="../assets/instagram.svg" width="32" height="32">
+                        </a>
                     </li>
                     <li>
-                        <router-link to="/">
-                            About
-                        </router-link>
+                        <a :href="`https://www.twitter.com/${userData.social_media.twitter}`" :title="`${userData.social_media.twitter}`" target="_blank" rel="noreferrer noopener">
+                            <img src="../assets/tweeter.svg" width="32" height="32">
+                        </a>
+                        
                     </li>
                     <li>
-                        <router-link to="/">
-                            Gallery
-                        </router-link>
+                        <a :href="`https://www.snapchat.com/${userData.social_media.snapchat}`" :title="`${userData.social_media.snapchat}`" target="_blank" rel="noreferrer noopener">
+                            <img src="../assets/snapchat.svg" width="32" height="32">
+                        </a>
+                        
                     </li>
                     <li>
-                        <router-link to="/">
-                            Contact
-                        </router-link>
+                        <a :href="`mailto:${userData.social_media.email}`" :title="`${userData.social_media.email}`">
+                            <img src="../assets/envelope.svg" width="32" height="32">
+                        </a>
+                        
                     </li>
+                    
                 </ul>
             </div>
         </div>
-        <div class="sticky-container">
-        <ul class="sticky">
-            <li>
-                <a :href="`https://www.instagram.com/${userData.social_media.instagram}`" :title="`${userData.social_media.instagram}`" target="_blank" rel="noreferrer noopener">
-                    <img src="../assets/instagram.svg" width="32" height="32">
-                </a>
-            </li>
-            <li>
-                <a :href="`https://www.twitter.com/${userData.social_media.twitter}`" :title="`${userData.social_media.twitter}`" target="_blank" rel="noreferrer noopener">
-                    <img src="../assets/tweeter.svg" width="32" height="32">
-                </a>
-                
-            </li>
-            <li>
-                <a :href="`https://www.snapchat.com/${userData.social_media.snapchat}`" :title="`${userData.social_media.snapchat}`" target="_blank" rel="noreferrer noopener">
-                    <img src="../assets/snapchat.svg" width="32" height="32">
-                </a>
-                
-            </li>
-            <li>
-                <a :href="`mailto:${userData.social_media.email}`" :title="`${userData.social_media.email}`">
-                    <img src="../assets/envelope.svg" width="32" height="32">
-                </a>
-                
-            </li>
-            
-        </ul>
-</div>
     </div>
 </template>
 
@@ -74,22 +79,36 @@
         name: "landing",
         data(){
             return{
-                link:'www.facebook.com',
-                userData: null
+                userData: null,
+                loading: false
             }
         },
         methods: {
-            getResponse() {
-                axios.get('https://hirng-x2021.glitch.me/api')
-                .then(response => {
+            async getResponse() {
+
+                // Please wait while loading is true
+                this.loading = true
+
+                try {
+                    const response = await axios.get('https://hirng-x2021.glitch.me/api')
+
+                    // Loading is done
+                    this.loading = false
+                    
+                    // Assign response data to userData
                     this.userData = response.data
-                    console.log(response);
-                }).catch(error => {
-                    console.log(error);
-                })
+                } catch (error) {
+
+                    // Loading done
+                    this.loading = false
+
+                    // If any error exists log in on the console
+                    console.log(error.response);
+                }
             }
         },
         mounted() {
+            // Call method once page is ready
             this.getResponse()
         }
     }
